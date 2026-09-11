8b4fb6c082054a0989854e21411fc061032a5bbf
ZaneYu7 <zongzeyu183@gmail.com>
2026-09-11 21:48:34 +0800

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/jobflow/JobFlowApplication.java
@@ -0,0 +1,13 @@
+package com.jobflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobFlowApplication.class, args);
+	}
+
+}
